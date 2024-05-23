314996f80f7a7a7f5768771097ea0801ca45c183
Jeffrey Cheng <chengjef@oregonstate.edu>
2024-05-22 23:31:36 -0400

    added user input to enter for client type.

--- test_congrats.py
+++ test_congrats.py
@@ -2,7 +2,7 @@
 # OSU Email: chengjef@oregonstate.edu
 # Course: CS361 - Software Engineering 1
 # Due Date: 06MAY2024 @ 11:59 PM PST. Using grace days, new due date is 08MAY2024 @ 11:59 PM PST.
-# Description: Congratulations message microservice program.
+# Description: Congratulations message microservice testing program.
 
 import time
 import zmq
@@ -10,7 +10,7 @@ import json
 
 def test_main():
     """
-    Client main function
+    Client main function for testing program.
     """
 
     # Create a socket on the client side and connect to "tcp://*:5555"
@@ -20,15 +20,36 @@ def test_main():
     socket.connect("tcp://localhost:5555")
     print(f"Connection established.")
 
+    # Get user input to which client they will be running.
+    user_inp = input("What client type will you be using: ")
+    if user_inp.lower() == "trivia":
+        valid = True
+    elif user_inp.lower() == "workout" or user_inp.lower() == "expense":
+        valid = True
+    else:
+        valid = False
+
+    # Loop until a valid client type is input.
+    while valid is False:
+        user_inp = input("What client type will you be using: ")
+        if user_inp.lower() == "trivia":
+            valid = True
+        elif user_inp.lower() == "workout" or user_inp.lower() == "expense":
+            valid = True
+        else:
+            valid = False
+
+    # Create JSON Object to send to Server/Microservice
     client_json = {
-        "client_type" : "trivia",
+        "client_type" : user_inp,
         "correct" : False
     }
 
+    # Send JSON object from client to microservice.
     socket.send_json(client_json)
+    print(f"Client sending to server: {client_json}")
 
-    print(f"Sending: {client_json}")
-
+    # Receive a JSON object back from the microservice.
     server_json = socket.recv_json()
     print(f"Receiving: {server_json}")
 
