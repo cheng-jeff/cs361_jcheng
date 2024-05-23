d22c691f1fbeac9d94062ddeb88b71f737720c4e
Jeffrey Cheng <chengjef@oregonstate.edu>
2024-05-23 00:26:38 -0400

    updated receive statement on test_congrats.py

--- test_congrats.py
+++ test_congrats.py
@@ -87,7 +87,7 @@ def test_main():
 
     # Receive a JSON object back from the microservice.
     server_json = socket.recv_json()
-    print(f"Receiving: {server_json}")
+    print(f"Client receiving from server: {server_json}")
 
 if __name__ == "__main__":
     test_main()
